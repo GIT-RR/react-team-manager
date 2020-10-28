0cd69c9075c0e651cca34cfcb9daa6054d9d9a27
rui.ribeiro <>
2020-10-28 00:19:46 +0000

    add tests

---
+++ src/components/pages/LoginPage/Login.page.spec.js
@@ -0,0 +1,10 @@
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import LoginPage from './LoginPage';
+import { get } from 'react-hook-form';
+
+describe('LoginPage', () => {
+  it('fields should be empty', () => {
+    expect(1).toEqual(1);
+  });
+});
